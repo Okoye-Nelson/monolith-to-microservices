bce16bd8879f39c87edcc95dc41f4bdd28db87bb
SudKul <sudhanshu.kulshrestha@gmail.com>
2021-07-05 14:59:17 +0530

    Travis build trigger

---
+++ udagram-api-user/src/server.ts
@@ -0,0 +1,55 @@
+import cors from 'cors';
+import express from 'express';
+import {sequelize} from './sequelize';
+
+import {IndexRouter} from './controllers/v0/index.router';
+
+import bodyParser from 'body-parser';
+import {config} from './config/config';
+import {V0MODELS} from './controllers/v0/model.index';
+
+const c = config;
+
+(async () => {
+  await sequelize.addModels(V0MODELS);
+  await sequelize.sync();
+
+  const app = express();
+  const port = process.env.PORT || 8080;
+
+  // Enable All CORS Requests
+  app.use(cors())
+  
+  /*
+  app.use(cors({
+    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token", "Authorization"],
+    methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+    origin: c.url
+  }));
+  */
+
+  /*
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Access-Token, Authorization");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, POST, OPTIONS, PUT, PATCH, DELETE');
+    //res.header("Access-Control-Allow-Origin", "http://localhost:8100");
+    next();
+  });
+  */
+
+  app.use(bodyParser.json());
+  app.use('/api/v0/', IndexRouter)
+
+  // Root URI call
+  app.get( "/", async ( req, res ) => {
+    res.send( "/api/v0/" );
+  } );
+
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running ` + c.url );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
