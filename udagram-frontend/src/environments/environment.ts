aa5f2627c2aa6d863ca8be7b84d1c85742882555
sudkul <sudhanshu.kulshrestha@gmail.com>
2021-08-04 19:33:35 +0530

    Working copy

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aeaee3399fb934e35973c359039e2230-999088717.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
